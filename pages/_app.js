d07f2dd173c3b8dc011dd38f293d371aaf46480e
juliobritojimenez <julio@treli.co>
2023-01-30 19:50:58 -0500

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -6,13 +6,8 @@ const spartan = League_Spartan({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
-      <style jsx global>{`
-        html {
-          font-family: ${spartan.style.fontFamily};
-        }
-      `}</style>
+    <main className={spartan}>
       <Component {...pageProps} />
-    </>
+    </main>
   )
 }
