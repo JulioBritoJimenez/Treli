219e07394793a0428cc7c9a2bf3148dab0dadd12
juliobritojimenez <julio@treli.co>
2023-02-09 20:31:44 -0500

    commit

---
+++ pages/account/coupons/coupon.js
@@ -0,0 +1,14 @@
+import Layout from "../../../componentes/layout";
+
+
+function NuevoCupon() {
+  return(
+    <>
+      <Layout>
+        <h2>Nuevo cupón</h2>
+      </Layout>
+    </>
+  )
+}
+
+export default NuevoCupon;
