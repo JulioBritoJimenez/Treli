3d58f05bf129dfacc67518ae45500ad42dc9eaf1
juliobritojimenez <julio@treli.co>
2023-04-03 17:37:27 -0500

    commit

--- pages/account/my-account/team.js
+++ pages/account/my-account/team.js
@@ -12,7 +12,7 @@ function Team() {
           <Link href="/account/my-account/team" className="settings-head-item plist-activestyle">Equipo</Link>
         </div>
 
-        <div className="team_filter input input-blanco flex">
+        <div className="team_filter input input-blanco flex margin-bottom">
           <div className="team-filter_name hidden">
             <div id="subs-search-customer" className="subs-search-customer">
               <input type="text" placeholder="Search member" issubs="yes" id="customer-search" name="customer-search" />
@@ -30,6 +30,29 @@ function Team() {
           <div className="add_team_member">
             <button className="botones boton-violeta button-full">Agregar miembro</button>
           </div>
+        </div>
+
+        <div className="team_table order-box-container">
+          <table className="payment-history">
+            <tbody>
+              <tr>
+                <th>Nombre</th>
+                <th>Correo electrónico</th>
+                <th>Rol</th>
+                <th>Última vez visto</th>
+                <th>Estado</th>
+                <th className="t_table_right"></th>
+              </tr>
+              <tr>
+                <td className="td-long">Pedro Perez - 12312312</td>
+                <td>demo@treli.co</td>
+                <td className="capitalize">Corredor</td>
+                <td>22 de marzo de 2023 07:53 PM</td>
+                <td className="capitalize">Activo</td>
+                <td className="t_table_right"></td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </Layout>
     </>
