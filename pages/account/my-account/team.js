cf57ec2115dddb0732c3094a2710a33c59cbd1a8
juliobritojimenez <julio@treli.co>
2023-04-04 15:41:22 -0500

    commit

--- pages/account/my-account/team.js
+++ pages/account/my-account/team.js
@@ -1,7 +1,19 @@
 import Layout from "../../../componentes/layout";
 import Link from 'next/link';
+import { BsPencilSquare, BsX } from "react-icons/bs";
+import { Button, Modal } from 'antd';
+import { useState } from 'react';
 
 function Team() {
+
+  const [miembro, setMiembro] = useState(false);
+  const showMiembro = () => {
+    setMiembro(true);
+  };
+  const handleMiembro = () => {
+    setMiembro(false);
+  };
+
   return(
     <>
       <Layout>
@@ -28,9 +40,70 @@ function Team() {
           </div>
 
           <div className="add_team_member">
-            <button className="botones boton-violeta button-full">Agregar miembro</button>
+            <button className="botones boton-violeta button-full" onClick={showMiembro}>Agregar miembro</button>
           </div>
         </div>
+
+        <Modal 
+          title="Nuevo miembro"
+          open={miembro} 
+          onCancel={handleMiembro} 
+          footer={[<Button onClick={handleMiembro} key="agregar">Agregar</Button>]} closeIcon={<BsX />} 
+          centered width={700}
+        >
+          <div className="miembro-equipo">
+            <p className="app-input">
+              <label className="acc-label">Nombre</label>
+              <input type="text" className="v-required" name="name" id="name" placeholder="Nombre" />
+            </p>
+            <p className="app-input">
+              <label className="acc-label">Apellido</label>
+              <input type="text" className="v-required" name="last_name" id="last_name" placeholder="Apellido" />
+            </p>
+            <p className="app-input">
+              <label className="acc-label">Correo electrónico</label>
+              <input type="text" className="v-required" name="email" id="email" placeholder="Email" />
+            </p>
+            <p className="app-input">
+              <label className="acc-label">Teléfono</label>
+              <input type="number" className="v-required" name="buphone" id="buphone" placeholder="Teléfono" />
+            </p>
+            <p className="app-input">
+              <label className="acc-label">Referencia interna</label>
+              <input type="text" className="v-required" name="internal_reference" id="internal_reference" placeholder="11223344" />
+            </p>
+            <p className="app-input">
+              <label className="acc-label">Rol</label>
+              <select className="v-required" name="create_member_role" id="create_member_role">
+                <option value="">Selecciona el rol</option>
+                <option value="corredor">Corredor</option>
+                <option value="analista">Analista</option>
+              </select>
+            </p>
+            <p className="app-input siigo_vendor_cont hidden">
+              <label className="acc-label">Vendedor Siigo</label>
+              <select name="siigo_vendor" id="siigo_vendor">
+                <option value="">Selecciona Vendedor</option>
+                <option value="vendedor1">Vendedor #1</option>
+                <option value="vendedor2">Vendedor #2</option>
+              </select>
+            </p>
+            <p className="app-input">
+              <label className="acc-label">Preferencia de login</label>
+              <select name="login_type" id="login_type">
+                <option value="otp">OTP</option>
+                <option value="contrasena">Contraseña</option>
+              </select>
+            </p>
+            <p className="app-input">
+              <label className="acc-label">Estado</label>
+              <select name="status" id="status">
+                <option value="activo">Activo</option>
+                <option value="inactivo">Inactivo</option>
+              </select>
+            </p>
+          </div>
+        </Modal>
 
         <div className="team_table order-box-container">
           <table className="payment-history">
@@ -49,7 +122,15 @@ function Team() {
                 <td className="capitalize">Corredor</td>
                 <td>22 de marzo de 2023 07:53 PM</td>
                 <td className="capitalize">Activo</td>
-                <td className="t_table_right"></td>
+                <td className="t_table_right"><BsPencilSquare className="editar-miembro" onClick={showMiembro} /></td>
+              </tr>
+              <tr>
+                <td className="td-long">Prueba Prueba - 23145667</td>
+                <td>demo@treli.co</td>
+                <td className="capitalize">Corredor</td>
+                <td>22 de marzo de 2023 07:53 PM</td>
+                <td className="capitalize">Activo</td>
+                <td className="t_table_right"><BsPencilSquare className="editar-miembro" onClick={showMiembro} /></td>
               </tr>
             </tbody>
           </table>
